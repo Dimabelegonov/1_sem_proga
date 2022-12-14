bb152def05fa112b053a1fc1bf334280f1e93985
merkulovsvt <merkulov.svttt@gmail.com>
2022-12-14 20:11:31 +0300

    228

--- Зачет/10. Проверка простоты числа. Метод грубой силы.py
+++ Зачет/10. Проверка простоты числа. Метод грубой силы.py
@@ -1,5 +1,4 @@
 "Метод грубой силы - Brute force"
-
 def bf(n):
     d = 2
     while n % d != 0:
@@ -13,13 +12,11 @@ print(bf(int(input())))
 
 
 "Быстрый способ"
-
 def func(n):
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
-
 
 print(func(int(input())))
 
