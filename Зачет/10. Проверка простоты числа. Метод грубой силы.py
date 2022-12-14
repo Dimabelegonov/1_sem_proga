0aaad05ff4671e4fefabb9732d69530c72c88857
dbelegonov <dima_belegonov@mail.ru>
2022-12-15 02:25:51 +0300

    до 11 билета

--- Зачет/10. Проверка простоты числа. Метод грубой силы.py
+++ Зачет/10. Проверка простоты числа. Метод грубой силы.py
@@ -1,23 +1,27 @@
-"Метод грубой силы - Brute force"
-def bf(n):
-    d = 2
-    while n % d != 0:
-        d = d + 1
-    return d == n
+"""Метод грубой силы - Brute force"""
 
 
-print(bf(int(input())))
+# Программа проверяет простое ли число методом грубой силы, т.е проверяет делится ли оно на любое чило
+# меньшее заданого
 
-"Временная сложность алгоритма - O(n)"
+
+def is_simple(x):
+    divisor = 2
+    while divisor < x:
+        if x % divisor == 0:
+            return False
+        divisor += 1
+    return True
 
 
-"Быстрый способ"
 def func(n):
+    """Быстрый способ"""
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
 
+
 print(func(int(input())))
 
-"Временная сложность алгоритма - O(sqrt(n))"
+"""Временная сложность алгоритма - O(sqrt(n))"""
