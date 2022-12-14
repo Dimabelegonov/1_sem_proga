5863fbc0e7fb994e26f000eb64c46f01c7bc6f79
merkulovsvt <merkulov.svttt@gmail.com>
2022-12-14 21:00:38 +0300

    228

--- Зачет/20. Поразрядная сортировка.py
+++ Зачет/20. Поразрядная сортировка.py
@@ -1,14 +1,14 @@
 def func(n):
     length = len(str(max(n)))
-    a = [[] for _ in range(10)]
+    A = [list() for _ in range(10)]
     for i in range(length):
-        for j in range(len(n)):
-            ch = n[j] // 10 ** i % 10
-            a[ch].append(n[j])
-        n = []
-        for j in range(10):
-            n = n + a[j]
-        a = [[] for _ in range(10)]
+        for x in n:
+            A[x // 10 ** i % 10].append(x)
+        n.clear()
+        for l in A:
+            for x in l:
+                n.append(x)
+            l.clear()
     return n
 
 data = list(map(int, input().split()))
