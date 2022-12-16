b439f00f31b29c542e8654374ad4ca386d944ec7
merkulovsvt <merkulov.svttt@gmail.com>
2022-12-16 20:06:50 +0300

    228

--- Свят/Зачет Свята/40. Z-функция.py
+++ Свят/Зачет Свята/40. Z-функция.py
@@ -28,17 +28,22 @@ print(z1(list(input())))
 Временная сложность алгоритма - O(n**2)
 """
 
+
 def z2(s):
     z = [0] * len(s)
-    l=0
+    l = 0
     for i in range(1, len(s)):
-        z[i] = min(z[i-l],l+z[l]-1)
-        z[i]=max(z[i],0)
+        z[i] = min(z[i - l], l + z[l] - 1)
+        z[i] = max(z[i], 0)
         while i + z[i] < len(s) and s[i + z[i]] == s[z[i]]:
             z[i] += 1
-        if i+z[i]>l+z[l]:
-          l=i
+        if i + z[i] > l + z[l]:
+            l = i
     return z
 
 
 print(z2(list(input())))
+
+"""
+Временная сложность алгоритма - O(n)
+"""
