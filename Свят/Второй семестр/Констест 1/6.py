0f5ea532d7f4ef5376dda0e01ca2d297e989effa
merkulovsvt <merkulov.svttt@gmail.com>
2023-02-14 17:29:25 +0300

    228

--- Свят/Второй семестр/Констест 1/6.py
+++ Свят/Второй семестр/Констест 1/6.py
@@ -3,14 +3,12 @@ def func(a):
     for i in range(len(a)):
         for j in range(len(a) - i - 1):
             if a[j] > a[j + 1]:
-                tmp=a[j]
-                a[j]=a[j+1]
-                a[j+1]=tmp
-
+                a[j], a[j + 1] = a[j + 1], a[j]
                 print(" ".join(map(str, a)))
             else:
                 continue
 
 
-a = input().split()
+a = list(map(int, input().split()))
+
 func(a)
